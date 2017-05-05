3b6fb3e431f1217ad57a414103a6d6549aac3710
Zac Ambrose <zambrose2017@gmail.com>
2017-05-05 09:06:56 -0600

    created seed file with sample data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,19 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+10.times do |blog|
+  Blog.create!(
+    title: "My Post #{blog}",
+    body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+    )
+end
+
+puts "10 blog post created"
+
+9.times do |video_link|
+  Video.create!(
+    title: "video title: #{}", 
+    body:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book." , 
+    main_image: "http://placehold.it/600x400", 
+    thumb_image: "http://placehold.it/350x200"
+    )
+
+end
+puts "10 video links created"
