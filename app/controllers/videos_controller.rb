fcb546febefbb165e20c9614086666e2b973bb84
Zac Ambrose <zambrose2017@gmail.com>
2017-05-08 16:10:18 -0600

    integreated new and create actions of vid-feature

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -2,9 +2,20 @@ class VideosController < ApplicationController
   def index
     @video_items = Video.all
   end
+  def new
+    @video_item = Video.new
+  end
+  def create
+    @video = Video.new(params.require(:video).permit(:title, :body))
 
-
-
+    respond_to do |format|
+      if @video.save
+        format.html { redirect_to videos_path, notice: 'Video portfolio was successfully created.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
 
 
 
