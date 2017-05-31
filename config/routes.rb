aed9b59ce547e00ec10629a9a6f4cdd638a7cd93
Zac Ambrose <zambrose2017@gmail.com>
2017-05-31 08:30:24 -0600

    added toggle status to blogs controller, implementing functionality to toggle posts between draft and published

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,11 @@ Rails.application.routes.draw do
   get 'nutrition', to: 'pages#nutrition'
   get 'faq', to: 'pages#faq'
 
-  resources :blogs
+  resources :blogs do
+    member do
+      get :toggle_status
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to:'pages#home'
 
